3cd50946a9376236d59b50e9064abec278ce2671
Denys Kushnarov <154005444+reginleif888@users.noreply.github.com>
2024-09-02 17:33:28 +0200

    Improve build process in docker
    Hash now is 60f7d2f8a5496f4fdd336377be77318d90c332e85cb59081f2ce9a3660b15d34

--- docker/build.sh
+++ docker/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 $(mops toolchain bin moc) src/main.mo -o out.wasm -c --debug --public-metadata candid:service --public-metadata candid:args $(mops sources)
+ic-wasm out.wasm -o out.wasm shrink
 sha256sum out.wasm
 
