0a887620ecfaf965c8ffd3884710f81f47d132c4
Denys Kushnarov <154005444+reginleif888@users.noreply.github.com>
2024-09-03 17:14:38 +0200

    Apply custom .did file

--- docker/build.sh
+++ docker/build.sh
@@ -2,5 +2,8 @@
 
 $(mops toolchain bin moc) src/main.mo -o out.wasm -c --debug --public-metadata candid:service --public-metadata candid:args $(mops sources)
 ic-wasm out.wasm -o out.wasm shrink
+if [ -f service.did ]; then
+    ic-wasm out.wasm -o out.wasm metadata candid:service -f service.did -v public
+fi
 sha256sum out.wasm
 
